cbce7471e8d203db9259dc3187c4cfcca41dfdf9
ivanleongwm <ivanleongwm@gmail.com>
2022-03-24 15:46:05 +0800

    buttons mapped in footerbar

--- src/views/FooterBar/FooterBar.jsx
+++ src/views/FooterBar/FooterBar.jsx
@@ -1,18 +1,36 @@
 import './FooterBar.css'
 import FooterButton from './FooterButton'
 import portfolio from './Icons/portfolio.png'
+import buy from './Icons/buy.png'
+import sell from './Icons/sell.png'
 
 const buttons = [
     {
         name: 'Portfolio',
         icon: portfolio,
+    },
+    {
+        name: 'Buy',
+        icon: buy
+    },
+    {
+        name: 'Sell',
+        icon: sell
     }
 ]
 
 export default function FooterBar() {
     return (
         <div className="footerFixedElement">
-            <FooterButton buttonData={buttons[0]}/>
+            <div className="button-container">
+                {
+                    buttons.map(
+                        element => {
+                            return <FooterButton buttonData={element}/>
+                        }
+                    )
+                }
+            </div>
         </div>
     )
 }
--- src/views/FooterBar/FooterButton.jsx
+++ src/views/FooterBar/FooterButton.jsx
@@ -1,9 +1,9 @@
 import './FooterButton.css'
 export default function FooterButton({buttonData}) {
     return (
-        <>
+        <div className="button">
             <img src={buttonData.icon}></img>
             <div>{buttonData.name}</div>
-        </>
+        </div>
     )
 }
