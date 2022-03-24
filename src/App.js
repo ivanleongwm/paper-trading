843368ad1068da6419c3aa80683da909568e3fcf
ivanleongwm <ivanleongwm@gmail.com>
2022-03-24 14:39:42 +0800

    header bar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import JunkContent from './views/JunkContent';
+import HeaderBar from './views/HeaderBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HeaderBar />
         <p>Hello</p>
+        <JunkContent />
       </header>
     </div>
   );
---
+++ src/views/HeaderBar.jsx
@@ -0,0 +1,8 @@
+import './HeaderBar.css'
+export default function HeaderBar() {
+    return (
+        <div className="fixedElement">
+            PAPER TRADE
+        </div>
+    )
+}
