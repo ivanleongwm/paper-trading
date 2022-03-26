274cf166a84654bfaf0ec8985920cf7dc85f3cf6
ivanleongwm <ivanleongwm@gmail.com>
2022-03-26 08:43:26 +0800

    raw charts added

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -1,10 +1,17 @@
 import TopSpacer from "./TopSpacer";
+import LineChart from "./LineChart";
+import PieChart from "./PieChart";
+import './Portfolio.css'
 
 export default function Portfolio() {
     return (
         <div>
             <TopSpacer/>
-            <div>Hello</div>
+            <div className="chart-container">
+                <PieChart />
+                <LineChart />
+            </div>
         </div>
     )
 }
+//<MixedChart />
