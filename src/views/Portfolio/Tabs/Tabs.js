bb10d099868e052992cd6f840f251f994a63bc7f
ivanleongwm <ivanleongwm@gmail.com>
2022-03-27 10:06:44 +0800

    tabs component added with swaping tab functionality

--- src/views/Portfolio/Tabs/Tabs.js
+++ src/views/Portfolio/Tabs/Tabs.js
@@ -1,16 +1,42 @@
+import React, { useState } from "react";
+import FirstTab from './AllTabs/FirstTab'
+import SecondTab from './AllTabs/SecondTab'
 import './Tabs.css'
 
 export default function Tabs() {
+    const [activeTab, setActiveTab] = useState("tab1");
+
+    //  Functions to handle Tab Switching
+    const handleTab1 = () => {
+        // update the state to tab1
+        setActiveTab("tab1");
+    };
+    const handleTab2 = () => {
+        // update the state to tab2
+        setActiveTab("tab2");
+    };
+
     return (
         <div className="Tabs">
             {/* Tab nav */}
             <ul className="nav">
-                <li>Tab 1</li>
-                <li>Tab 2</li>
+                <li
+                    className={activeTab === "tab1" ? "active" : ""}
+                    onClick={handleTab1}
+                >
+                    Tab 1
+                </li>
+                <li
+                    className={activeTab === "tab2" ? "active" : ""}
+                    onClick={handleTab2}
+                >
+                    Tab 2
+                </li>
             </ul>
             <div className="outlet">
-                {/* content will be shown here */}
+                {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
             </div>
         </div>
     )
 }
+
