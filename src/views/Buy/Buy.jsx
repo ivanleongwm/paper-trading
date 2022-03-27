743711333dea2dbfcf2c30a398c5d813f7320433
ivanleongwm <ivanleongwm@gmail.com>
2022-03-27 20:12:22 +0800

    buy stocks cards added

--- src/views/Buy/Buy.jsx
+++ src/views/Buy/Buy.jsx
@@ -1,11 +1,27 @@
 import TopSpacer from "../Header/TopSpacer";
 import stockHistoricalPrices from "../../model/stockHistoricalPrices"
+import BuyStockCard from "./BuyStockCard/BuyStockCard";
+import './Buy.css'
 
 export default function Buy() {
     return (
         <div>
             <TopSpacer />
-            <div>{stockHistoricalPrices.AAPL.name}</div>
+            <div className="buy-container">
+                {
+                    Object.keys(stockHistoricalPrices).map(function(key, index) {
+                        return (<BuyStockCard stockHistoricalPrices={stockHistoricalPrices[key]}/>);
+                      })
+                }
+            </div>
         </div>
     )
 }
+
+/*
+Object.keys(myObject).map(function(key, index) {
+  myObject[key] *= 2;
+});
+
+
+*/
--- src/views/Buy/BuyStockCard/BuyStockCard.jsx
+++ src/views/Buy/BuyStockCard/BuyStockCard.jsx
@@ -1,10 +1,15 @@
-export default function BuyStockCard () {
+import MiniChart from '../MiniChart/MiniChart'
+import './BuyStockCard.css'
+
+export default function BuyStockCard ({stockHistoricalPrices}) {
     return (
         <div className="card-container">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>{stockHistoricalPrices.name}</div>
+            <div>{stockHistoricalPrices.price}</div>
+            <div>{stockHistoricalPrices.twentyFourHourChange}</div>
+            <div>
+                <MiniChart historicalPrices={stockHistoricalPrices.historical}/>
+            </div>
         </div>
     )
 }
