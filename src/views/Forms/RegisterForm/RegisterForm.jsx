89c91928ad0e61277cdb6bebb5e4302dbb37678d
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-04-09 15:47:12 +0800

    registerform troubleshoot

--- src/views/Forms/RegisterForm/RegisterForm.jsx
+++ src/views/Forms/RegisterForm/RegisterForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import urlcat from "urlcat";
 import { BACKEND } from "../../../utils/utils";
 
-const url = urlcat(BACKEND, "/api/users");
+const url = urlcat(BACKEND, "/api/users/");
 
 function Register() {
   const [error, setError] = useState("");
