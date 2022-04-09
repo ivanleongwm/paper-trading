85be59184991647412e69d220eed3797976ac623
ivanleongwm <ivanleongwm@gmail.com>
2022-04-09 11:06:30 +0800

    test display of data fetched from heroku backend

--- src/views/Forms/FormDataDisplay/FormDataDisplay.jsx
+++ src/views/Forms/FormDataDisplay/FormDataDisplay.jsx
@@ -1,12 +1,27 @@
 import TopSpacer from '../../../views/Header/TopSpacer'
+import {useEffect,useState} from 'react';
+import urlcat from "urlcat";
+import {BACKEND} from "../../../utils/utils"
 
 export default function FormDataDisplay () {
+    const [userData, setUserData] = useState([]);
+
+    useEffect(()=> {
+        fetch(urlcat(BACKEND, "/api/Users/"))
+            .then((response) => response.json())
+            .then((data) => setUserData(data));
+    },[]);
+
+    console.log(userData)
+
+
     return (
         <>
             <TopSpacer/>
             <div>
                 Display Data
             </div>
+            <div>{userData}</div>
         </>
     )
 }
