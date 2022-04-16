06a423063f9e5b87b0d168a504374e3ccd53977b
ivanleongwm <ivanleongwm@gmail.com>
2022-04-16 15:16:16 +0800

    env

--- src/views/Buy/Buy.jsx
+++ src/views/Buy/Buy.jsx
@@ -2,9 +2,25 @@ import TopSpacer from "../Header/TopSpacer";
 import stockHistoricalPrices from "../../model/stockHistoricalPrices"
 import userAccountData from "../../model/userAccountData";
 import BuyStockCard from "./BuyStockCard/BuyStockCard";
-import './Buy.css'
+import './Buy.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 export default function Buy() {
+    const [stock, setStock] = useState({
+      })
+    
+      useEffect(()=> {
+        axios.get('https://financialmodelingprep.com/api/v3/historical-price-full/AAPL,AMZN,GOOG,MSFT,NVDA?apikey=ed422f5ab8a52bef7a04a8d39de5129d')
+        .then(res => {
+          const stockObject = res.data;
+          console.log(res.data.historicalStockList)
+          //console.log(stockObject[0])
+          setStock(stockObject)
+          console.log(stock)
+        })
+      },[])
+
     return (
         <div>
             <TopSpacer />
