95fe70001568d2628934802c7d3c6d80704ef56d
ivanleongwm <ivanleongwm@gmail.com>
2022-04-23 00:34:52 +0800

    duplicated buy components to sell component

--- src/views/Sell/Sell.jsx
+++ src/views/Sell/Sell.jsx
@@ -1,10 +1,117 @@
 import TopSpacer from "../Header/TopSpacer";
+import stockHistoricalPrices from "../../model/stockHistoricalPrices"
+import userAccountData from "../../model/userAccountData";
+import BuyStockCard from "./BuyStockCard/BuyStockCard";
+import './Buy.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import urlcat from "urlcat";
+import { BACKEND } from "../../utils/utils";
 
-export default function Sell() {
+export default function Buy({ secret, setSecret }) {
+    const [stock, setStock] = useState([])
+    const [nasdaq,setNasdaq] = useState("")
+    const [secret2, setSecret2] = useState({
+        user: "",
+        purchaseLog:[]
+      });
+
+    const url = urlcat(BACKEND, "/api/users/loginsuccessful");
+
+    const loginSuccessCheck = () => {
+        fetch(url, {
+          method: "GET",
+          credentials: 'include',
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => {
+              console.log(response)
+            return response.json()
+          })
+          .then((data) => {
+            console.log("first data",data)
+            setSecret2({ ...secret2, user: data.username, purchaseLog: data.purchaseLog })
+          })
+          .catch((error) => console.log(error));
+      };
+    
+      useEffect(() => {
+        loginSuccessCheck()
+      },[])
+    
+      useEffect(()=> {
+        axios.get('https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=ed422f5ab8a52bef7a04a8d39de5129d')
+        .then(res => {
+          const nasdaqRawObject = res.data;
+          const listOfNasdaqCompanies = []
+          console.log(res.data)
+          nasdaqRawObject.map((x,i) => {
+            listOfNasdaqCompanies.push(nasdaqRawObject[i].symbol)
+          })
+          console.log("list of nasdaq companies",listOfNasdaqCompanies.join(","))
+          setNasdaq(listOfNasdaqCompanies)
+          console.log(nasdaq)
+        })
+      },[])
+
+      useEffect(() => {
+
+        const mainCompanyStockCall = async () => {
+            const stockPromises = []
+            const stockResults = []
+            const cleanedStockResults = []
+            
+            for (let i = 0; i < 10; i++) {
+                stockPromises.push(
+                    axios.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${nasdaq.slice(1*i*5,5+i*5).join(",")}?apikey=ed422f5ab8a52bef7a04a8d39de5129d`)
+                )
+              }
+
+            const stockResponses = await Promise.allSettled(stockPromises)
+            console.log('stockresponses',stockResponses)
+
+            stockResponses.forEach((resp,index) => {
+                stockResults.push(resp.value.data)
+              })
+            console.log("resolved api calls",stockResults)
+            
+            for (const stockbatch of stockResults) {
+                for (const stock of stockbatch.historicalStockList) {
+                    cleanedStockResults.push(stock)
+                }
+            }
+
+            function compare( a, b ) {
+                if ( a.symbol < b.symbol ){
+                  return -1;
+                }
+                if ( a.symbol > b.symbol ){
+                  return 1;
+                }
+                return 0;
+              }
+              
+            cleanedStockResults.sort( compare );
+            console.log("cleaned sorted stock list",cleanedStockResults)
+            setStock(cleanedStockResults)
+        }
+        mainCompanyStockCall()
+      },[nasdaq])
+
+    console.log("secret state from other component",secret)
+
     return (
         <div>
             <TopSpacer />
-            <div>SELL</div>
+            <div className="buy-container">
+                {
+                    stock.map((x, i) => {
+                        return (<BuyStockCard stockHistoricalPrices={stock[i]} userAccountData={userAccountData} secret={secret} setSecret={setSecret} username={secret2.user}/>);
+                      })
+                }
+            </div>
         </div>
     )
 }
