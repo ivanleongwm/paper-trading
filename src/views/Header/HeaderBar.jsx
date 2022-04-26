b81835300b706b89725e703b3321796ef91421e0
ivanleongwm <ivanleongwm@gmail.com>
2022-04-26 12:56:42 +0800

    added login and logged in user

--- src/views/Header/HeaderBar.jsx
+++ src/views/Header/HeaderBar.jsx
@@ -2,8 +2,20 @@ import './HeaderBar.css'
 
 export default function HeaderBar() {
     return (
-        <div className="fixedElement">
-            PAPER TRADE
+        <div className="headerContainer">
+            <div className="fixedElement">
+                <div className="loggedInUser">
+                    Logged In User: {sessionStorage.getItem("username")}
+                </div>
+                <div className="title">
+                    PAPER TRADE
+                </div>
+                <div className="links">
+                    <a href="/login">Login</a>
+                    <a href="/register">Register</a>
+                </div>
+            </div>
         </div>
+        
     )
 }
